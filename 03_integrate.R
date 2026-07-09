85b4d62d0639d29b430aabe747998867db48e9ef
yeonwoo818 <syeonu818@gmail.com>
2026-07-09 15:03:52 +0900

    Update 03_integrate.R

--- 03_integrate.R
+++ 03_integrate.R
@@ -1,9 +1,9 @@
 # Data integration and UMAP Drawing
 # 1. Memory Clearing
 rm(
-  data1.data, 
-  data1.seurat, 
-  data1.seurat.v3, 
+  data1.data, ...
+  data1.seurat, ...
+  data1.seurat.v3, ...
   sweep.res, sweep.stats, bcmvn,
   best.pK, nExp, homotypic.prop, nExp.adj,
   df.col1
