bc61e0a17c2f1e12cfa404369ed68642cb529735
yeonwoo818 <syeonu818@gmail.com>
2026-07-09 14:20:01 +0900

    Update 03_integrate.R

--- 03_integrate.R
+++ 03_integrate.R
@@ -12,3 +12,34 @@ gc()
 
 # 2. Data Merging
 data1.singlet$Origin <- "Sample1"
+...
+data10.singlet$Origin <- "Sample10"
+merged.seurat <- merge(x = data1.singlet, 
+                       y = list(data2.singlet, ... , data10.singlet),
+                       add.cell.ids = c("Sa_1", ..., "Sa_10"),
+                       project = "Samples_merged"
+                       )
+
+
+# 3. Normalization + Batch Correction
+merged.seurat <- SCTransform(
+  merged.seurat, 
+  assay = "RNA",
+  new.assay.name = "SCT",
+  method = "glmGamPoi"
+)
+DefaultAssay(merged.seurat) <- "SCT"
+merged.seurat <- RunPCA(merged.seurat, assay = "SCT", npcs = 10)
+merged.seurat <- RunHarmony(
+  merged.seurat,
+  group.by.vars = "Origin"
+  #assay.use = "SCT"
+)
+merged.seurat <- RunUMAP(merged.seurat, reduction = "harmony", dims = 1:10)
+merged.seurat <- FindNeighbors(merged.seurat, reduction = "harmony", dims = 1:10)
+merged.seurat <- FindClusters(merged.seurat, resolution = 0.6)
+DimPlot(merged.seurat, reduction = "umap", group.by = "Origin")
+DimPlot(merged.seurat, reduction = "umap", label = TRUE)
+
+
+
